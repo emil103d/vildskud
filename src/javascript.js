931fb607c2674475ee38779a131b31e2f8f339fd
Soph0298 <Soph0298@stud.kea.dk>
2022-04-28 11:31:51 +0200

    popup done mangler margin auto

--- src/javascript.js
+++ src/javascript.js
@@ -1,27 +1,17 @@
+const popup = document.querySelector("#popup");
 window.addEventListener("load", SidenVises);
 
 function SidenVises() {
   console.log("Siden vises");
   document.querySelector("#popup").classList.add("show");
   document.querySelector("#billetyper").classList.add("fade");
+  document.querySelector("#popup").addEventListener("click", lukPopup);
 }
 
-// function popUp() {
-//     console.log("Popup");
-//     hjaelp.classList.add("skjul");
-//     popup.classList.remove("skjul");
-//     popup.classList.add("show");
-//     play.classList.remove("skjul");
-//     play.classList.add("pulse");
-//     play.addEventListener("click", startSpil);
-
-// }
-//       }
-// document.querySelector("#start_background").classList.add("right");
-// logo.classList.add("show");
-// hjaelp.classList.add("pulse");
-// hjaelp.addEventListener("click", popUp);
-// lvlcomplskaerm.classList.add("skjul");
-// gameoverskaerm.classList.add("skjul");
-// popup.classList.add("skjul");
-// play.classList.add("skjul");
+function lukPopup() {
+  console.log("Popupluk");
+  popup.classList.remove("show");
+  document.querySelector("#billetyper").classList.remove("fade");
+  // document.querySelector("#popup").;
+  // stener("click", startSpil);
+}
